aab9c982d84ac526c0d659bc8e9a5322a15920f7
Noah Smith <71087047+Ploverums@users.noreply.github.com>
2022-11-28 10:19:33 -0600

    Create FirstTimeScripts.py

---
+++ FirstTimeScripts.py
@@ -0,0 +1,13 @@
+print ("Hello World") 
+//basic hello world script to start
+for x in range(11):
+  print(x)
+  import time
+  time.sleep(1)
+//first usage of iterations and importing modules
+number = int(input("Enter a number to check if it's even or odd: "))
+if (number % 2) == 0:
+   print("{0} is Even".format(num))
+else:
+   print("{0} is Odd".format(num))
+//first usage of input, if/else statements, % operators, and assignment. make sure to expain these in detail when showing programs to the audience
