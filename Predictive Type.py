dc9e5a95549fde7acc750d5edeca7180cc9c2661
Noah Smith <71087047+Ploverums@users.noreply.github.com>
2022-12-13 16:18:30 -0600

    Update Predictive Type.py

--- Predictive Type.py
+++ Predictive Type.py
@@ -3,13 +3,13 @@ from nltk.tokenize import RegexpTokenizer
 from keras.models import Sequential, load_model
 from keras.layers import LSTM
 from keras.layers.core import Dense, Activation
-from keras.optimizers import RMSprop
 import matplotlib.pyplot as plt
+from tensorflow.keras.optimizers import RMSprop
 import pickle
 import heapq
 
-path = 'dev.json'
-text = open(path).read().lower()
+path = "x/x/x/train.txt"
+text = open(path, encoding='utf8').read().lower()
 
 tokenizer = RegexpTokenizer(r'\w+')
 words = tokenizer.tokenize(text)
@@ -23,13 +23,14 @@ BackWords = 5
 for i in range(len(words) - BackWords):
   last_words.append(words[i:i + BackWords])
   after_words.append(words[i + BackWords])
-  
-X = np.zeros((len(prev_words), WORD_LENGTH, len(unique_words)), dtype=bool)
-Y = np.zeros((len(next_words), len(unique_words)), dtype=bool)
-for i, each_words in enumerate(prev_words):
+print(last_words[0])
+print(after_words[0])  
+X = np.zeros((len(last_words), BackWords, len(su_words)), dtype=bool)
+Y = np.zeros((len(after_words), len(su_words)), dtype=bool)
+for i, each_words in enumerate(last_words):
     for j, each_word in enumerate(each_words):
-        X[i, j, unique_word_index[each_word]] = 1
-    Y[i, unique_word_index[next_words[i]]] = 1
+        X[i, j, su_word_index[each_word]] = 1
+    Y[i, su_word_index[after_words[i]]] = 1
     
 model = Sequential()
 model.add(LSTM(128, input_shape=(BackWords, len(su_words))))
@@ -44,3 +45,25 @@ model.save('keras_next_word_model.h5')
 pickle.dump(history, open("history.p", "wb"))
 model = load_model('keras_next_word_model.h5')
 history = pickle.load(open("history.p", "rb"))
+
+def prepare_input(text):
+    x = np.zeros((1, BackWords, len(su_words)))
+    for t, word in enumerate(text.split()):
+        x[0, t, su_word_index[word]] = 1.
+    return x
+
+def sample(preds, top_n=3):
+    preds = np.asarray(preds).astype('float64')
+    preds = np.log(preds)
+    exp_preds = np.exp(preds)
+    preds = exp_preds / np.sum(exp_preds)
+    return heapq.nlargest(top_n, range(len(preds)), preds.take)
+
+def predict_completion(text, n=3):
+    if text == "":
+        return("0")
+    x = prepare_input(text)
+    preds = model.predict(x, verbose=0)[0]
+    next_indices = sample(preds, n)
+    return [su_words[idx] for idx in next_indices]
+
